30b5562f20760605b3036a1b4ba1c9cc476e370a
lorenammp <lorenammpaula@gmail.com>
2024-04-30 20:25:28 -0300

    finalizado modal para visualização de detalhes na lista de fornecedores do cliente

--- codigo-fonte/js/script.js
+++ codigo-fonte/js/script.js
@@ -1,11 +1,57 @@
-// FUNÇÕES PARA ABRIR E FECHAR O MENU DE USUÁRIO
+import clientes from "../json/clientes.json" with {type: "json"};
 
-function openNav() {
-  document.getElementById("hiddenNav").style.height = "100px";
-  document.getElementById("header").style.marginTop = "100px";
-}
+console.log(typeof clientes.listaDeClientes);
 
-function closeNav() {
-  document.getElementById("hiddenNav").style.height = "0";
-  document.getElementById("header").style.marginTop = "0";
-}
+
+clientes.listaDeClientes.forEach((objetoCliente) => {
+    console.log(objetoCliente.nome);
+    console.log(objetoCliente.cnpj)
+})
+
+let counterItensPedido = 0;
+
+const btnAddItem = document.getElementById("add-btn-item-pedido");
+btnAddItem.addEventListener("click", function() {
+    let itemPedido = document.getElementById("add-item-pedido").value;
+    let qtdItemPedido = document.getElementById("add-qtd-item-pedido").value;
+
+    const listaItensPedido = document.getElementById("lista-itens-pedido");
+    
+    const item = document.createElement("li");
+    const nomeItem = document.createElement("div");
+    const qtdItem = document.createElement("div");
+    const closeBtnContainer = document.createElement("div");
+    const closeBtn = document.createElement("i");
+
+    // item.setAttribute("class", "itens-pedido-cadastrados");
+    item.classList.add("itens-pedido-cadastrados", "faded-out")
+    item.setAttribute("id", "id-" + counterItensPedido);
+    nomeItem.setAttribute("class", "ipc-1");
+    qtdItem.setAttribute("class", "ipc-1");
+    closeBtnContainer.setAttribute("class", "item-close-icon");
+    closeBtnContainer.setAttribute("id", counterItensPedido);
+    closeBtn.classList.add("fa-solid", "fa-xmark")
+
+    nomeItem.innerText = itemPedido;
+    qtdItem.innerText = qtdItemPedido;
+
+    item.appendChild(nomeItem);
+    item.appendChild(qtdItem);
+    closeBtnContainer.appendChild(closeBtn);
+    item.appendChild(closeBtnContainer);
+
+    listaItensPedido.appendChild(item);
+
+    requestAnimationFrame(() => {
+        item.classList.remove("faded-out");
+    })
+
+    const btnRemoveItem = document.getElementById(counterItensPedido);
+    btnRemoveItem.addEventListener("click", function() {
+        const itemLista = document.getElementById("id-" + btnRemoveItem.id);
+        itemLista.remove();
+    })
+
+    counterItensPedido++;
+
+});
