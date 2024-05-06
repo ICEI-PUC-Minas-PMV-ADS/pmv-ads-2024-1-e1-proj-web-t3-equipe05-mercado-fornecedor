92a604a9df4ebe9151ad1c5692706664e03fca0c
lorenammp <lorenammpaula@gmail.com>
2024-05-05 23:44:41 -0300

    ajustes de estilo e inclusão de novos inputs na página de cadastro

--- codigo-fonte/js/script.js
+++ codigo-fonte/js/script.js
@@ -3,25 +3,3 @@ import { listarPedidosPorCliente } from "./listarpedidos.js";
 
 const userData = JSON.parse(localStorage.getItem("User"));
 listarPedidosPorCliente(userData.id);
-
-// async function buscaCEP(cep) {
-//   const endereco = await Api.consultaCEP(cep);
-//   console.log(endereco);
-// }
-
-// static async consultaCEP(cep) {
-//     const response = await fetch("https://viacep.com.br/ws/" + cep + "/json", {
-//       method: "GET",
-//       mode: "cors",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     })
-//       .then((res) => res.json())
-//       .then((res) => {
-//         console.log(res);
-//         return res;
-//       });
-
-//     return response;
-//   }
