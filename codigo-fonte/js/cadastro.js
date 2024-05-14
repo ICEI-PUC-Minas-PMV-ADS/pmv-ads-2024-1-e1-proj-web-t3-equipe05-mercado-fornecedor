b0799f2fd458c00465917ce945a0b87bf9204733
lorenammp <lorenammpaula@gmail.com>
2024-05-14 11:37:07 -0300

    ajustes de estilo no form de cadastro; remoção de inputs desnecessários no form de cadastro; responsividade nos forms de cadastro e login

--- codigo-fonte/js/cadastro.js
+++ codigo-fonte/js/cadastro.js
@@ -1,43 +1,43 @@
 import Api from "./api.js";
 
 //BUSCA ENDEREÇO PELO CEP E PREENCHE AUTOMATICAMENTE OS INPUTS
-const btnCepCadastro = document.getElementById("buscar-btn-cep-cadastro");
-const objEndereco = {};
+// const btnCepCadastro = document.getElementById("buscar-btn-cep-cadastro");
+// const objEndereco = {};
 
-const exibirEndereco = (endereco) => {
-  for (const campo in endereco) {
-    if (document.getElementById(campo)) {
-      let input = document.getElementById(campo);
-      input.value = endereco[campo];
-      objEndereco[campo] = endereco[campo];
-    }
-  }
-};
+// const exibirEndereco = (endereco) => {
+//   for (const campo in endereco) {
+//     if (document.getElementById(campo)) {
+//       let input = document.getElementById(campo);
+//       input.value = endereco[campo];
+//       objEndereco[campo] = endereco[campo];
+//     }
+//   }
+// };
 
-btnCepCadastro.addEventListener("click", (e) => {
-  const cepCadastro = document.getElementById("cep-cadastro");
-  let cep = cepCadastro.value.replace("-", "");
+// btnCepCadastro.addEventListener("click", (e) => {
+//   const cepCadastro = document.getElementById("cep-cadastro");
+//   let cep = cepCadastro.value.replace("-", "");
 
-  const options = {
-    method: "GET",
-    mode: "cors",
-    cache: "default",
-  };
+//   const options = {
+//     method: "GET",
+//     mode: "cors",
+//     cache: "default",
+//   };
 
-  fetch(`https://viacep.com.br/ws/${cep}/json/`, options)
-    .then((response) => {
-      response.json().then((data) => exibirEndereco(data));
-    })
-    .catch((e) => console.log(e.message));
-});
+//   fetch(`https://viacep.com.br/ws/${cep}/json/`, options)
+//     .then((response) => {
+//       response.json().then((data) => exibirEndereco(data));
+//     })
+//     .catch((e) => console.log(e.message));
+// });
 
 function cadastrarUsuario() {
   const nome = document.getElementById("cadastro-nome").value;
   const cnpj = document.getElementById("cadastro-cnpj").value;
-  const complementoEnd = document.getElementById("cadastro-complemento").value;
-  const numeroEnd = document.getElementById("cadastro-numero").value;
-  const telefone = document.getElementById("cadastro-telefone").value;
-  const marcaImg = document.getElementById("cadastro-imgUrl").value;
+  // const complementoEnd = document.getElementById("cadastro-complemento").value;
+  // const numeroEnd = document.getElementById("cadastro-numero").value;
+  // const telefone = document.getElementById("cadastro-telefone").value;
+  // const marcaImg = document.getElementById("cadastro-imgUrl").value;
   const email = document.getElementById("cadastro-email").value;
   const senha = document.getElementById("cadastro-senha").value;
 
@@ -47,16 +47,17 @@ function cadastrarUsuario() {
     if (radio.checked) radioChecked = radio.value;
   }
 
-  objEndereco.complemento = complementoEnd;
-  objEndereco.numero = numeroEnd;
+  // objEndereco.complemento = complementoEnd;
+  // objEndereco.numero = numeroEnd;
 
   const objUser = {
     nome: nome,
     cnpj: cnpj,
     tipo: radioChecked,
-    endereco: objEndereco,
-    telefone: telefone,
-    imgUrl: marcaImg,
+    segmento: null,
+    endereco: null,
+    telefone: null,
+    imgUrl: null,
     email: email,
     password: senha,
   };
@@ -67,6 +68,7 @@ function cadastrarUsuario() {
 const btnCadastrarUser = document.getElementById("cadastrar-user-btn");
 
 btnCadastrarUser.addEventListener("click", (e) => {
+  e.preventDefault();
   cadastrarUsuario();
-  document.getElementById("form-cadastro-usuario").reset();
+  // document.getElementById("form-cadastro-usuario").reset();
 });
