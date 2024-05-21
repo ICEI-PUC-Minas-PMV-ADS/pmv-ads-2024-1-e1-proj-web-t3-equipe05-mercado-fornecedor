f2eb3e75d0864d3405c28ad50b380078a7a97664
lorenammp <lorenammpaula@gmail.com>
2024-05-21 00:15:00 -0300

    finalizada a funcionalidade de edição de dados na página minhaconta.html; ajustes menores nas funcionalidades de listar e cadastrar pedidos e em meus fornecedores

--- codigo-fonte/js/login.js
+++ codigo-fonte/js/login.js
@@ -1,7 +1,7 @@
 import Api from "./api.js";
 
 const objLogin = {
-  email: "contato@acrilex.com",
+  email: "contato@usiminas.com",
   password: "123456",
 };
 
@@ -9,6 +9,17 @@ export async function logUser() {
   const dados = await Api.login(objLogin);
 
   const userData = JSON.parse(localStorage.getItem("User"));
+
+  const userNavbar = document.getElementById("user-navbar");
+
+  const minhaConta = document.createElement("a");
+  const minhaContaSpan = document.createElement("span");
+
+  minhaContaSpan.innerText = "Minha conta";
+  minhaConta.href = "./minhaconta.html";
+
+  minhaConta.append(minhaContaSpan);
+  userNavbar.append(minhaConta);
 }
 
 logUser();
