3d1b5a96706e21dd603c1988784528c9ecabf10a
ReAndradec <165438649+ReAndradec@users.noreply.github.com>
2024-06-08 12:50:54 -0300

    Update login.js

--- codigo-fonte/js/login.js
+++ codigo-fonte/js/login.js
@@ -1,17 +1,37 @@
-import Api from "./api.js";
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let email = document.getElementById("login-input-email").value
+    let senha = document.getElementById("login-input-senha").value
 
-const objLogin = {
-  email: "contato@inova.com",
-  password: "123456",
-};
+    let formularioCompleto = [email, senha]
+    console.log(formularioCompleto); 
+} )
 
-export async function logUser() {
-  const isUserLogged = JSON.parse(localStorage.getItem("User"));
-  if (isUserLogged === null) {
-    const dados = await Api.login(objLogin);
-  }
-  const users = await Api.listarUsuarios();
-  localStorage.setItem("Users", JSON.stringify(users));
-}
-
-logUser();
+//     // Chamada à API de autenticação
+    fetch('./api.js', {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formularioCompleto)
+    })
+    .then(response => response.json())
+    .then(data => {
+    if (data.success) {
+//             // Autenticação bem sucedida
+    console.log('Login bem sucedido!');
+//             // Redireciona para a página principal ou dashboard
+    window.location.href = './index.html';
+    } 
+// else {
+//             // Autenticação falhou
+//             console.log('Falha no login: ', data.message);
+//             // Exibir mensagem de erro ao usuário
+//             document.getElementById('error message').innerText = data.message;
+//         }
+//     })
+//     .catch(error => {
+//         console.error('Erro:', error);
+//         document.getElementById('error-message').innerText = 'Ocorreu um erro. Tente novamente mais tarde.';
+//     });
+// });
