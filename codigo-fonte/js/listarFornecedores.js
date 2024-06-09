9fa8297eaeb36f5b77e68f6b618bced259fcd98f
otavio stefanine <otaviostefanine@gmail.com>
2024-06-09 17:40:24 -0300

    listar fornecedores

--- codigo-fonte/js/listarFornecedores.js
+++ codigo-fonte/js/listarFornecedores.js
@@ -1,22 +1,21 @@
 // LISTA OS FORNECEDORES NA PÁGINA FORNECEDORES.HTML
 function listarFornecedores() {
-    const listaDeFornecedores = JSON.parse(localStorage.getItem("Fornecedores"));
-  
-    const ul = document.getElementById("lista-de-fornecedores");
-    listaDeFornecedores.forEach((f) => {
-      const li = document.createElement("li");
-      li.classList.add("card-fornecedor");
-  
-      li.innerHTML = `
-      <img src="${f.imgUrl}" class="card-img-top" alt="${f.nome}" />
-        <div class="card-body">
-          <h5 class="card-fornecedor-title">${f.nome}</h5>
-          <p class="card-fornecedor-text">${f.segmento}</p>
-        </div>`;
-  
-      ul.append(li);
-    });
-  }
-  
-  listarFornecedores();
-  
+  const listaDeFornecedores = JSON.parse(localStorage.getItem("Fornecedores"));
+
+  const ul = document.getElementById("lista-de-fornecedores");
+  listaDeFornecedores.forEach((f) => {
+    const li = document.createElement("li");
+    li.classList.add("card-fornecedor");
+
+    li.innerHTML = `
+    <img src="${f.imgUrl}" class="card-img-top" alt="${f.nome}" />
+      <div class="card-body">
+        <h5 class="card-fornecedor-title">${f.nome}</h5>
+        <p class="card-fornecedor-text">${f.segmento}</p>
+      </div>`;
+
+    ul.append(li);
+  });
+}
+
+listarFornecedores();
